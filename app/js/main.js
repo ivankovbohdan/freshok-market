5822d60c41b52585167a1528616edb771a100b82
IvaBod <bodanich600@gmail.com>
2022-08-27 14:20:51 +0100

    started project, added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
-$(function() {
-  
+$(function () {
+
+  $(".header__catalog-btn").on("click", function () {
+
+    $('.header__catalog-btn').toggleClass('header__catalog-btn--active');
+    $('.header__catalog-list').toggleClass('header__catalog-list--active');
+
+  });
+
+  $(".catalog-menu__link").on("click", function () {
+
+    $('.header__catalog-link').toggleClass('header__catalog-link--active');
+    $('.header__catalog-item').toggleClass('header__catalog-item--active');
+
+  });
+
 })
